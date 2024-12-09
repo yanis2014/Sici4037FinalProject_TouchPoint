ddc878d638cc1c1884b34fc8304a4a30b8f3839c
yanis2014 <tatiana.rodriguez101@upr.edu>
2024-12-08 22:27:25 -0400

    added a server connection window

--- tats/version2/gui.py
+++ tats/version2/gui.py
@@ -6,6 +6,7 @@ import client
 
 from threading import Timer
 
+# Check if entry is a valid IP
 def is_valid_ip(ip):
     try:
         splt = ip.split(".")
@@ -16,24 +17,26 @@ def is_valid_ip(ip):
         return len(splt) == 4
     except:
         return False
-    
+
+# Check if entry is a valid port
 def is_valid_port(port):
     try:
         elem = int(port)
         return (0 <= elem <= 65535)
     except:
         return False
-    
 
+# Clears the error labels
 def clear_err_labels():
-    err_label1.configure(text="")
-    err_label2.configure(text="")
-    err_label3.configure(text="")
+    op1_ip_err_label.configure(text="")
+    op1_port_err_label.configure(text="")
+    op2_port_err_label.configure(text="")
 
+# Clears the entries
 def clear_entries():
-    entry1.delete(0,tk.END)
-    entry2.delete(0,tk.END)
-    entry3.delete(0,tk.END)
+    op1_ip_entry.delete(0,tk.END)
+    op1_port_entry.delete(0,tk.END)
+    op2_port_entry.delete(0,tk.END)
 
 # Function to enable/disable input fields based on selected radio button
 def toggle_inputs():
@@ -44,33 +47,34 @@ def toggle_inputs():
     else: 
         state1 = "disabled"
         state2 = "normal"
-    entry1.configure(state=state1)
-    entry2.configure(state=state1)
-    entry3.configure(state=state2)
-    label1.configure(state=state1)
-    label2.configure(state=state1)
-    label3.configure(state=state2)
+    op1_ip_entry.configure(state=state1)
+    op1_port_entry.configure(state=state1)
+    op2_port_entry.configure(state=state2)
+    op1_ip_label.configure(state=state1)
+    op1_port_label.configure(state=state1)
+    op2_port_label.configure(state=state2)
 
+# Returns true if invalid entry is found
 def validate_entries(op1_ip, op1_port, op2_port):
     # Initialize invalid entry flag
     invalid_entry = False
 
     # Validates the ip entry if option 1 is enabled
-    if entry1.cget("state") == "normal" and not is_valid_ip(op1_ip):
-        err_label1.configure(text=f" * Invalid IP Address: '{op1_ip}'")
-        entry1.delete(0, tk.END)
+    if op1_ip_entry.cget("state") == "normal" and not is_valid_ip(op1_ip):
+        op1_ip_err_label.configure(text=f" * Invalid IP Address: '{op1_ip}'")
+        op1_ip_entry.delete(0, tk.END)
         invalid_entry = True
     
     # Validates the ip entry if option 1 is enabled
-    if entry2.cget("state") == "normal" and not is_valid_port(op1_port):
-        err_label2.configure(text=f" * Invalid Port: '{op1_port}'")
-        entry2.delete(0, tk.END)
+    if op1_port_entry.cget("state") == "normal" and not is_valid_port(op1_port):
+        op1_port_err_label.configure(text=f" * Invalid Port: '{op1_port}'")
+        op1_port_entry.delete(0, tk.END)
         invalid_entry = True
     
     # Validates the ip entry if option 1 is enabled
-    if entry3.cget("state") == "normal" and not is_valid_port(op2_port):
-        err_label3.configure(text=f" * Invalid Port: '{op2_port}'")
-        entry3.delete(0, tk.END)
+    if op2_port_entry.cget("state") == "normal" and not is_valid_port(op2_port):
+        op2_port_err_label.configure(text=f" * Invalid Port: '{op2_port}'")
+        op2_port_entry.delete(0, tk.END)
         invalid_entry = True
 
     return invalid_entry
@@ -82,24 +86,43 @@ def click_connect():
     clear_err_labels()
 
     # Get connection details
-    op1_ip = entry1.get()
-    op1_port = entry2.get()
-    op2_ip = socket.gethostbyname(socket.gethostname())
-    op2_port = entry3.get()
+    op1_ip = op1_ip_entry.get()
+    op1_port = op1_port_entry.get()
+    op2_port = op2_port_entry.get()
 
     # Terminate if invalid entry
-    if validate_entries():
+    if validate_entries(op1_ip, op1_port, op2_port):
         return
 
     # Client option selected
     if selected_option.get() == "Option 1":
+        
+        # Close the gui window
         window.destroy()
+
+        # Run client program
         client.client_program(hostserver=op1_ip, port=int(op1_port))
     
     # Server option selected
     else:
-        
-        server.server_program(port=int(op2_port))
+        # Open listening window
+        server_connect_window(op2_port)
+
+        # Run server program
+        server.server_program(port=int(op2_port), window=window)
+
+def server_connect_window(port):
+    # Clear the current frame
+    for widget in main_frame.winfo_children():
+        widget.pack_forget()
+
+    # Edit status text
+    server_connect_status.configure(text=f"{socket.gethostbyname(socket.gethostname())} is listening on port {port}...")
+    
+    # Place widgets
+    server_connect_header.pack(anchor="w", padx=10, pady=30)
+    server_connect_status.pack(anchor="w", padx=10)
+    cancel_button.pack(anchor="w", padx=10)
 
 def main_window():
     # Clear the current frame
@@ -109,24 +132,35 @@ def main_window():
     # Clear entries
     clear_entries()
 
+    # Put input and error labels next to each other
+    op1_ip_entry.grid(row=0, column=0)
+    op1_ip_err_label.grid(row=0, column=1, padx= 10)
+
+    op1_port_entry.grid(row=0, column=0)
+    op1_port_err_label.grid(row=0, column=1, padx= 10)
+
+    op2_port_entry.grid(row=0, column=0)
+    op2_port_err_label.grid(row=0, column=1, padx= 10)
+
     # Place main window widgets
     instruction_label.pack(anchor="w", pady=10, padx=10)
 
     radio_buttons["Option 1"].pack(anchor="w", pady=10, padx=10)
-    label1.pack(anchor="w", padx=35)
-    ip_frame.pack(anchor="w", padx=35)
-    label2.pack(anchor="w", padx=35)
-    port1_frame.pack(anchor="w", padx=35)
+    op1_ip_label.pack(anchor="w", padx=35)
+    op1_ip_frame.pack(anchor="w", padx=35)
+    op1_port_label.pack(anchor="w", padx=35)
+    op1_port_frame.pack(anchor="w", padx=35)
 
     radio_buttons["Option 2"].pack(anchor="w", pady=10, padx=10)
-    label3.pack(anchor="w", padx=35)
-    port2_frame.pack(anchor="w", padx=35)
+    op2_port_label.pack(anchor="w", padx=35)
+    op2_port_frame.pack(anchor="w", padx=35)
 
     connect_button.pack(anchor="w", pady=20, padx=20)
 
     toggle_inputs()  # Reset the inputs based on the current selection
 
 if __name__ == "__main__":
+    
     # Create the main window
     window = tk.Tk()
 
@@ -141,47 +175,77 @@ if __name__ == "__main__":
     main_frame.pack(fill="both", expand=True)
 
     # Create instruction label
-    instruction_label = tk.Label(main_frame, text="Please choose an option below and enter the corresponding information:")
+    instruction_label = tk.Label(
+        main_frame, 
+        text="Please choose an option below and enter the corresponding information:")
 
     # Stores the selected option
     selected_option = tk.StringVar(value="Option 1")
 
     # Create two radio buttons
     radio_buttons = {}
-    radio_buttons["Option 1"] = tk.Radiobutton(main_frame, text="Connect to Another Computer", value="Option 1", variable=selected_option, command=toggle_inputs)
-    radio_buttons["Option 2"]= tk.Radiobutton(main_frame, text="Allow Connection to Your Computer", value="Option 2", variable=selected_option, command=toggle_inputs)
+    radio_buttons["Option 1"] = tk.Radiobutton(
+        main_frame, 
+        text="Connect to Another Computer", 
+        value="Option 1", 
+        variable=selected_option, 
+        command=toggle_inputs)
+    radio_buttons["Option 2"]= tk.Radiobutton(
+        main_frame, 
+        text="Allow Connection to Your Computer", 
+        value="Option 2", 
+        variable=selected_option, 
+        command=toggle_inputs)
 
     # Create labels
-    label1 = tk.Label(main_frame, text="IP Address:")
-    label2 = tk.Label(main_frame, text="Port:")
-    label3 = tk.Label(main_frame, text="Port:")
+    op1_ip_label = tk.Label(main_frame, text="IP Address:")
+    op1_port_label = tk.Label(main_frame, text="Port:")
+    op2_port_label = tk.Label(main_frame, text="Port:")
 
     # Create frames for input and error labels
-    ip_frame = tk.Frame(main_frame)
-    port1_frame = tk.Frame(main_frame)
-    port2_frame = tk.Frame(main_frame)
+    op1_ip_frame = tk.Frame(main_frame)
+    op1_port_frame = tk.Frame(main_frame)
+    op2_port_frame = tk.Frame(main_frame)
 
-    # Create input and error labels
-    entry1 = tk.Entry(ip_frame)
-    err_label1 = tk.Label(ip_frame, text="", fg="red")
+    # Create entry boxes
+    op1_ip_entry = tk.Entry(op1_ip_frame)
+    op1_port_entry = tk.Entry(op1_port_frame)
+    op2_port_entry = tk.Entry(op2_port_frame)
 
-    entry2 = tk.Entry(port1_frame)
-    err_label2 = tk.Label(port1_frame, text="", fg="red")
+    # Create error labels
+    op1_port_err_label = tk.Label(op1_port_frame, text="", fg="red")
+    op1_ip_err_label = tk.Label(op1_ip_frame, text="", fg="red")
+    op2_port_err_label = tk.Label(op2_port_frame, text="", fg="red")
 
-    entry3 = tk.Entry(port2_frame)
-    err_label3 = tk.Label(port2_frame, text="", fg="red")
+    # Create Connect button
+    connect_button = tk.Button(
+        main_frame, 
+        text="Connect", 
+        bg="blue", 
+        fg="white", 
+        command=click_connect
+    )
 
-    # Put input and error labels next to each other
-    entry1.grid(row=0, column=0)
-    err_label1.grid(row=0, column=1, padx= 10)
 
-    entry2.grid(row=0, column=0)
-    err_label2.grid(row=0, column=1, padx= 10)
+    # Create server connection header label
+    server_connect_header = tk.Label(
+        main_frame, 
+        text=radio_buttons["Option 2"].cget("text"), 
+        font = ('Calibri', 16))
 
-    entry3.grid(row=0, column=0)
-    err_label3.grid(row=0, column=1, padx= 10)
+    # Create server connection status label
+    server_connect_status = tk.Label(main_frame)
 
-    # # Initial state of inputs (enabled for Option 1)
+    # Create cancel button
+    cancel_button = tk.Button(
+        main_frame, 
+        text="Cancel", 
+        bg="red", 
+        fg="white" #,
+        #command
+    )
+
+    # Initial state of inputs (enabled for Option 1)
     toggle_inputs()
 
     # Create Connect button
